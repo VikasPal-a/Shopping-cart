4a1b11b37d9e44ce781096d4708343144cb3635d
Vikas <tumharaemail@gmail.com>
2025-12-08 08:01:44 -0800

    first commit

---
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController {
+	@GetMapping("/index")
+	public String index () { 
+		return"index" ;
+	     }
+		
+		
+		@GetMapping("/login")
+	public String login () { 
+		return"login";
+	       }
+		@GetMapping("/Register")
+   public String Register () { 
+			return"Register";
+}
+}
